c6e345e8f867de560e8fd037216b8cdba8cd6d44
christylall <lchristy245@gmail.com>
2026-04-14 17:33:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_API_KEY_HERE";
+const API_KEY = "da287b27ab2c62083846949656a915d4";
 
 const homeSection = document.getElementById("homeSection");
 const searchBtn = document.getElementById("searchBtn");
